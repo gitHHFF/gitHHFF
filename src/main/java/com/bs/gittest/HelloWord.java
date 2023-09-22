881ee94be2078ce5e581410597f725be1ca044ea
gitHHFF <2233789160@qq.com>
2023-09-22 17:22:23 +0800

    first commit

--- src/main/java/com/bs/gittest/HelloWord.java
+++ src/main/java/com/bs/gittest/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HEllo GIT");
         System.out.println("Hello GIT");
+        System.out.println("Hello GIT");
     }
 }
